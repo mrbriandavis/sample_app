2e6fd9149f774d3772894f65c7be3fbc2827715a
bdavis <mrbriandavis@outlook.com>
2013-09-01 09:56:33 -0700

    4.5. finished

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,9 @@
 require 'spec_helper'
 
 describe "Static pages" do
-	#let(:base_title) {"Ruby on Rails Tutorial Sample App"}
 
 	describe "Home page" do
-		
+
 		it "should have the h1 'Sample App'" do
 			visit '/static_pages/home' 
 			page.should have_selector('h1', :text => 'Sample App') 
