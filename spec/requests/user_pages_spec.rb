8b2a55baa093430ac94f247bff684a135fdecad3
bdavis <mrbriandavis@outlook.com>
2013-09-14 23:19:31 -0700

    end of chapter 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -22,6 +22,7 @@ describe "profile page" do
 end
 
 describe "signup" do
+	
 	before { visit signup_path }
 
 	let(:submit) {"Create my account"}
@@ -30,7 +31,15 @@ describe "signup" do
 		it "should not create a user" do
 			expect  {click_button submit }.not_to change(User, :count)
 		end
+
+	describe "after submission" do
+		before	{ click_button submit }
+
+		it { should have_selector('title', text: 'Sign up')}
+		it { should have_content('error') }
+		it { should_not have_content('Password digest') }
 	end
+end
 
 	describe "with valid information" do
 		before do
@@ -40,12 +49,18 @@ describe "signup" do
 			fill_in "Confirmation", 	with: "foobar"
 		end
 
-		it "should crate a user" do
+		it "should create a user" do
 			expect {click_button submit }.to change(User, :count).by(1)
 		end
+
+	describe "after saving the user" do
+		before { click_button submit }
+
+		let(:user) { User.find_by_email('example@example.com') }
+
+		it { should have_selector('title', text: user.name) }
+		it { should have_selector('div.alert.alert-success', text: 'Welcome') }
 	end
+  end
 end
-
 end
-
-
